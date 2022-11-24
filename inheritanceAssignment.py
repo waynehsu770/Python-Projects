9e10b3f5228306ccdea1b4d7b174103eece8b8d2
waynehsu770 <waynehsu770@gmail.com>
2022-11-24 11:04:45 +0800

    Create inheritanceAssignment.py

---
+++ inheritanceAssignment.py
@@ -0,0 +1,14 @@
+
+
+class Vehicle:
+    name = ''
+    price = ''
+    seats = ''
+
+class Car(Vehicle):
+    wheels = True
+    brand = ''
+
+class Boat(Vehicle):
+    poweredBy = ''
+    registered = ''
